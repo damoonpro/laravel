4bb3833e0210efbae150e3eef73f6bf37e32ebbd
matio-nora <matio.nora2000@gmail.com>
2022-08-03 20:48:28 +0430

    feat-factory: user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,10 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->safeEmail(),
+            'mobile' => fake()->phoneNumber,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'mobile_verified_at' => now(),
+            'is_staff' => mt_rand(0, 5) > 4,
             'remember_token' => Str::random(10),
         ];
     }
@@ -36,6 +38,7 @@ class UserFactory extends Factory
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
+                'mobile_verified_at' => null,
             ];
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(3)->create();
     }
 }
