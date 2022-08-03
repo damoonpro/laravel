a95c5e55dbaf300c8dfffa1b16b417ba96518b46
matio-nora <matio.nora2000@gmail.com>
2022-08-04 00:15:46 +0430

    feat-module-route: guest collect blogs

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -1,1 +1,8 @@
 <?php
+
+use App\Http\Controllers\api\v1\Blog\Guest\Controller as BlogController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('blog')->controller(BlogController::class)->group(function (){
+    Route::get('/', 'collect'); // collect blogs by paginate 12 for guest users
+});
