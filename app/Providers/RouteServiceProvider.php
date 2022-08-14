88c85dcf62058d3bc82a0281b1e9f77b74bf5c47
matio-nora <matio.nora2000@gmail.com>
2022-08-14 18:14:36 +0430

    config-route: config route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,9 +29,16 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
+            /*
+             * localhost:{port}/api. it only describe about damoon
+             */
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
+
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->group(base_path('routes/v1/api.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
---
+++ routes/v1/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Damoon\Tools\Custom\Route\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register api routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "api" middleware group. Now create something great!
+|
+*/
