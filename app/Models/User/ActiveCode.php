e1ce5bd30cf0214e9c518359ac4fd7b1b2ae318c
matio-nora <matio.nora2000@gmail.com>
2022-08-14 18:43:45 +0430

    feat-model: active code model and migration

---
+++ app/Models/User/ActiveCode.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\User;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ActiveCode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'code',
+        'expired_at',
+    ];
+
+    protected $casts = [
+        'expired_at' => 'datetime',
+    ];
+}
---
+++ database/migrations/2022_08_14_184120_create_active_codes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('active_codes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->integer('code');
+            $table->timestamp('expired_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('active_codes');
+    }
+};
